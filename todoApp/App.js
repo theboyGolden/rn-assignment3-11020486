5eca1fe72f6719117eb58ff1cd7a2e34fd2e4da1
theboyGolden <preciousstick15@gmail.com>
2024-05-29 21:55:17 +0000

    Created the head section of the app

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,20 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Welcome to my TO DO APP </Text>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+
+        <View style={styles.columnContainer}>
+            <View style={styles.column1}>
+              <Text style={styles.HelloDevs}>Hello Devs, </Text>
+              <Text style={styles.nooftasks}>14 tasks today</Text> 
+            </View>
+
+            <View style={styles.column2}>
+              <Text>  </Text>
+            </View>
+
+            <View style={styles.column3}>
+              <Text> <Image source={require('./assets/person.png')} style={styles.user} /> </Text>
+            </View>
+
+        </View>
+        
+        <View style={styles.workspace}>
+          <Text> Fuck this is the workpage </Text>
+        </View>
+        
       <StatusBar style="auto" />
-    </View>
+      </View>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
+  
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#F7F0E8',
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F7F0E8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  columnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+
+  HelloDevs: {
+    fontSize: 30,
+    fontFamily: 'Lato',
+    fontWeight: '700',
+    color: '#000',
+
+  },
+
+  column1: {
+    flex: 3,
+  },
+
+  column2: {
+    flex: 2,
+  },
+
+  column3: {
+    flex: 1,
+  },
+
+  user: {
+    backgroundColor: '#fff',
+    borderRadius: 50,
+  }
 });
